3ba2326c3c4163e540f42b2d5f152544f6980e61
Arvind-Adhithya <42761445+Arvind-Adhithya@users.noreply.github.com>
2019-07-16 20:47:24 +0530

    Tik Tac Toe game

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Game from './components/Game';
+//import logo from './logo.svg';
+import './App.css';
+
+function App()  {
+
+  return (
+    <Game />
+  );
+
+}
+
+export default App;
